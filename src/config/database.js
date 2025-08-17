868299370b8a57a9aaac19bca08165b59f6835c4
Javier <javierrumo2@gmail.com>
2025-08-17 19:18:09 +0200

    Ejercicio terminado

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,4 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        logging: false,
-    }
-);
-
-module.exports = sequelize;
-
-/*
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
@@ -38,4 +22,3 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
-*/
